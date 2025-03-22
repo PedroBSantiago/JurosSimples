4b0ffe9a82146e62abbe6b5ae7ab1dd18ab50ef5
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-21 22:35:02 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿double TaxaDeJuros,Capital,Tempo;
+
+double JurosSimples; 
+double Montante;
+
+Console.Write("Capital [c] (R$).......:");
+Capital = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Taxa de juros [i] (%)..:");
+TaxaDeJuros = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Tempo [t] (meses)......:");
+Tempo = Convert.ToInt32(Console.ReadLine());
+
+JurosSimples = Capital * TaxaDeJuros * Tempo;
+
+Montante = JurosSimples + Capital;
+
+Console.WriteLine($"Juros (R$).....:{Capital} *  {TaxaDeJuros} * {Tempo} = {JurosSimples}");
+
+Console.WriteLine($"Montante (R$)..:{Capital} +  {JurosSimples} = {Montante}");
